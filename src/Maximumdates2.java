95e441dd841b813f98b73b25e9313c938d1e16e4
jun <jun@192.168.0.8>
2018-01-24 20:01:56 +0900

    nogada ver1

--- src/Maximumdates2.java
+++ src/Maximumdates2.java
@@ -11,20 +11,48 @@ public class Maximumdates2 {
 		}
 	public static void main(String[] args) {
 		
-		Maximumdates2 maxx = new Maximumdates2();
 		Scanner scanner = new Scanner(System.in);
 		
+		int input_months = 1;
+		while(true) {
 		
-		System.out.println("반복 횟수를 력하세?");
-		int input_times = scanner.nextInt();
-		
-		for(int i = 0; i<input_times; i++) {
-			
 			System.out.println("달을 입력 하시오");
-			int input_months = scanner.nextInt();
-			System.out.println(input_months + "월의 날짜 수는 "+ maxx.maxDaysOfMonths(input_months)+"일 입니다");
-		}
+			input_months = scanner.nextInt();
+			
+			if (input_months == -1) {
+				break ;
+			} else if (input_months > 12) {
+				continue;
+			} else if (input_months == 2) {
+			
+				System.out.println("일 월 화 수 목 금 토");
+				System.out.println("-------------------");
+				System.out.println("1  2  3  4  5  6  7");
+				System.out.println("8  9  10 11 12 13 14");
+				System.out.println("15 16 17 18 19 20 21");
+				System.out.println("22 23 24 25 26 27 28");
+			} else if( input_months == 4 || input_months == 6 || input_months == 9 || input_months == 11) {
+				System.out.println("일 월 화 수 목 금 토");
+				System.out.println("-------------------");
+				System.out.println("1  2  3  4  5  6  7");
+				System.out.println("8  9  10 11 12 13 14");
+				System.out.println("15 16 17 18 19 20 21");
+				System.out.println("22 23 24 25 26 27 28");
+				System.out.println("29 30");
+			} else if( input_months == 1 || input_months == 3 || input_months == 5 || input_months == 7 || input_months == 8 || input_months == 10 || input_months == 12) {
+				System.out.println("일 월 화 수 목 금 토");
+				System.out.println("-------------------");
+				System.out.println("1  2  3  4  5  6  7");
+				System.out.println("8  9  10 11 12 13 14");
+				System.out.println("15 16 17 18 19 20 21");
+				System.out.println("22 23 24 25 26 27 28");
+				System.out.println("29 30 31");
+				
+			}
 		
+		
+	
+			}
 		scanner.close();
 	}
 }
