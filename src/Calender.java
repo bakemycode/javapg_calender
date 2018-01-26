9715b50cef6aaed46a9b86da41c3782fc03f8a36
jun <jun@192.168.0.8>
2018-01-26 14:51:56 +0900

    calendar,input(month,year,date)

--- src/Calender.java
+++ src/Calender.java
@@ -1,26 +1,58 @@
 public class Calender {
 
 	private final int[] MONTHS={31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	private final int[] LEAP_YEARS={31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 	
-	public int daysOfMonth(int month) 
-	{	 
-		return MONTHS[month - 1];
+	public int daysOfMonth(int year, int month) {
+		
+		if (isLeapYear(year) == false) {
+			return MONTHS[month - 1];
+		} else {
+			return LEAP_YEARS[month -1];
+		}
 	}
 	
-	public void makeCal(int months) {
-		System.out.printf("%8d 월",months);
+	public boolean isLeapYear(int year) {
+		if (year % 4 == 0 && year % 100 != 0 || (year % 100 == 0 && year % 400 == 0)) {
+			return true;
+			}
+		return false;
+		}
+			
+	
+	public void makeCal(int years, int months, int day) {
+		System.out.printf("%4d년  %3d월 ",years, months);
 		System.out.println();
 		System.out.println(" SU MO TU WE TH FR SA");
 		System.out.println("---------------------");
+	
+		for(int j = 1; j <= day; j++) {
+			System.out.print("   ");
+		}
+
+		for(int i = 1; i < (7+1) - day; i++ ) {	
+			
+			System.out.printf("%3d", i);
+			}
 		
-		for(int i = 1; i < daysOfMonth(months); i++ ) {
+		System.out.println();
+		
+			
+		for(int i = 7 - day + 1; i < daysOfMonth(years, months) + 1; i++ ) {	
+
 			System.out.printf("%3d", i);
 			
-			if(i % 7 == 0) {
-				System.out.println();
+			if(day == 0) {
+				if(i % 7 == 0) {
+					System.out.println();
+				}
 			}
-		}
-		System.out.println();
+			else if (day > 0) {
+				if(i % 7 == 7 - day)
+					System.out.println();
+				}
+			}
 		System.out.println();
 	}
-}
+	}
+
--- src/Prompt.java
+++ src/Prompt.java
@@ -5,17 +5,38 @@ public class Prompt {
 
 	private final static String PROMPT="cal> ";
 	
-	public static void runPrompt() {
+	public int parsingDay(String str_day) {
+		if (str_day.equals("SU")) return 0;
+		else if (str_day.equals("MO")) return 1;
+		else if (str_day.equals("TU")) return 2;
+		else if (str_day.equals("WE")) return 3;
+		else if (str_day.equals("TU")) return 4;
+		else if (str_day.equals("FR")) return 5;
+		else if (str_day.equals("SA")) return 6;
+		else 
+			return 0;
+		
+	}
+	
+	public void runPrompt() {
 		
 		Scanner scanner = new Scanner(System.in);
 		Calender cal = new Calender();
 		
+		int years;
+		int day = 1;
 		int months = 1;
+		
 		while(true) {
 		
 			System.out.println("달을 입력 하시오");
 			System.out.print(PROMPT);
 			months = scanner.nextInt();
+			
+			System.out.println("요일을 입력 하시오 (SU, MO, TU, WE, TH, FR, SA)");
+			System.out.print(PROMPT);
+			String str_day = scanner.next();
+			day = parsingDay(str_day);
 			
 			if (months == -1) {
 				break ;
@@ -23,14 +44,18 @@ public class Prompt {
 				continue;
 			} else {
 			
-			cal.makeCal(months);
+			System.out.println("년도를 입력 하시오");
+			System.out.print(PROMPT);
+			years = scanner.nextInt();
+			cal.makeCal(years, months, day);
 			
 			}
 		}
-		
+	scanner.close();	
 	}
 	
 	public static void main(String[] args) {
+		
 		Prompt p = new Prompt();
 		p.runPrompt();
 	}
