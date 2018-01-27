7ca08bf6e2208a7b7f2a0289b91894cebc224b46
jun <jun@192.168.0.8>
2018-01-27 22:35:14 +0900

    input only year and month

--- src/Calender.java
+++ src/Calender.java
@@ -1,14 +1,14 @@
 public class Calender {
 
-	private final int[] MONTHS={31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-	private final int[] LEAP_YEARS={31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	private final int[] MONTHS={0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	private final int[] LEAP_YEARS={0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 	
 	public int daysOfMonth(int year, int month) {
 		
 		if (isLeapYear(year) == false) {
-			return MONTHS[month - 1];
+			return MONTHS[month];
 		} else {
-			return LEAP_YEARS[month -1];
+			return LEAP_YEARS[month];
 		}
 	}
 	
@@ -19,50 +19,52 @@ public class Calender {
 		return false;
 		}
 			
-	public int CalFirst(int year) {
+	public int CalFirst(int year, int month) {
 
-		int first_day = 0;
+		int start_day = 5; // Thu
+		int start_month = 0;
+		int how_many_days = 0;
 		
-		System.out.println(year);
-		
-		int how_many_leeps = 0;
-		
-		for (int i = 0; i <= year; i++) {
-			if (i % 4 == 0 && i % 100 != 0 || (i % 100 == 0 && i % 400 == 0)) {
-				how_many_leeps += 1;
-				}	
+		for (int i = 1970; i < year; i++) {
+			if (isLeapYear(i) == true) {
+				how_many_days += 366;
+			} else if (isLeapYear(i) == false) {
+				how_many_days += 365;
+				}
 		}
-		System.out.println(how_many_leeps);
-		first_day = (how_many_leeps + year) % 7;
-		System.out.println(first_day);
-		return first_day;
+		
+		for (int i = 1; i < month; i++) {
+			start_month += daysOfMonth(year,i);
+		}
+		
+		start_day = (start_day + start_month + how_many_days) % 7;
+	
+		return start_day;
 	}
 	
 	
-//	public void makeCal(int year, int month) {
-//		System.out.printf("%4d년  %3d월 ",year, month);
-//		System.out.println();
-//		System.out.println(" SU MO TU WE TH FR SA");
-//		System.out.println("---------------------");
-//		
-//		for(int i = 1; i <= CalFirst(year) - 1; i++) {
-//			System.out.printf("   ");
-//		}
-//		for(int i = 1; i < daysOfMonth(year, month) + 1; i++ ) {	
-//
-//			System.out.printf("%3d", i);
-//			if ((CalFirst(year) - 1) % 7 == 0) {
-//				if(i % 7 == 0) {
-//					System.out.println();
-//					}
-//			} else if ( (CalFirst(year) - 1) % 7 > 0) {
-//				if ( i % 7 == CalFirst(year)) {
-//					System.out.println();
-//				}
-//				}
-//		System.out.println();
-//	System.out.println();
-//	}
-//}
-}
+	public void makeCal(int year, int month) {
+		System.out.printf("%4d년  %3d월 ",year, month);
+		System.out.println();
+		System.out.println(" SU MO TU WE TH FR SA");
+		System.out.println("---------------------");
+		
+		for(int i = 1; i < CalFirst(year,month); i++) {
+			System.out.printf("   ");
+		}
+		for(int i = 1; i < daysOfMonth(year, month) + 1; i++ ) {	
+
+			System.out.printf("%3d", i);
+			
+			if (7 - CalFirst(year,month) + 1 == i % 7) {
+				System.out.println();
+				
+				}
+				}
+	}
+	
+		
+		}
+
+
 
--- src/Prompt.java
+++ src/Prompt.java
@@ -17,21 +17,22 @@ public class Prompt {
 		
 		while(true) {
 		
-			System.out.println("달을 입력 하시오");
+			System.out.println("달을 입력 하시오 (종료 = -1)");
 			System.out.print(PROMPT);
 			month = scanner.nextInt();
 			
 			if (month == -1) {
 				break ;
-			} else if (month > 12) {
+			} else if (month > 12 || month < -1 || month == 0){
+				System.out.println("옳바른 값을 입력하시오 (1~12)");
 				continue;
 			} else {
 			
 			System.out.println("년도를 입력 하시오");
 			System.out.print(PROMPT);
 			year = scanner.nextInt();
-//			cal.makeCal(year, month);
- 			cal.CalFirst(year);
+			cal.makeCal(year, month);
+ 			cal.CalFirst(year, month);
  			
 			}
 		}
