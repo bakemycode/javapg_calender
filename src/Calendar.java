86ead86ac84434bc2f01cc90d06f8c6e88e4bc39
jun <jun@192.168.0.8>
2018-01-29 05:17:28 +0900

    update, my version

--- src/Calendar.java
+++ src/Calendar.java
@@ -1,70 +1,82 @@
-public class Calender {
+import java.util.HashMap;
 
-	private final int[] MONTHS={0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-	private final int[] LEAP_YEARS={0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-	
+public class Calendar {
+
+	private final int[] MONTHS = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	private final int[] LEAP_YEARS = { 0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
 	public int daysOfMonth(int year, int month) {
-		
+
 		if (isLeapYear(year) == false) {
 			return MONTHS[month];
 		} else {
 			return LEAP_YEARS[month];
 		}
 	}
-	
+
 	public boolean isLeapYear(int year) {
 		if (year % 4 == 0 && year % 100 != 0 || (year % 100 == 0 && year % 400 == 0)) {
 			return true;
-			}
-		return false;
 		}
-			
+		return false;
+	}
+
 	public int CalFirst(int year, int month) {
 
 		int start_day = 5; // Thu
 		int start_month = 0;
 		int how_many_days = 0;
-		
+
 		for (int i = 1970; i < year; i++) {
 			if (isLeapYear(i) == true) {
 				how_many_days += 366;
 			} else if (isLeapYear(i) == false) {
 				how_many_days += 365;
-				}
+			}
 		}
-		
+
 		for (int i = 1; i < month; i++) {
-			start_month += daysOfMonth(year,i);
+			start_month += daysOfMonth(year, i);
 		}
-		
+
 		start_day = (start_day + start_month + how_many_days) % 7;
-	
+
 		return start_day;
 	}
-	
-	
+
 	public void makeCal(int year, int month) {
-		System.out.printf("%4d년  %3d월 ",year, month);
+		System.out.printf("%4d년  %3d월 ", year, month);
 		System.out.println();
 		System.out.println(" SU MO TU WE TH FR SA");
 		System.out.println("---------------------");
-		
-		for(int i = 1; i < CalFirst(year,month); i++) {
+
+		for (int i = 1; i < CalFirst(year, month); i++) {
 			System.out.printf("   ");
 		}
-		for(int i = 1; i < daysOfMonth(year, month) + 1; i++ ) {	
+		for (int i = 1; i < daysOfMonth(year, month) + 1; i++) {
 
 			System.out.printf("%3d", i);
-			
-			if (7 - CalFirst(year,month) + 1 == i % 7) {
+
+			if (7 - CalFirst(year, month) + 1 == i % 7) {
 				System.out.println();
-				
-				}
+
 			}
+		}
+		
+		System.out.println();
+	}
+
+	public void registerPlan(String strDate, String strPlan) {
+	
+		HashMap<String, String> map = new HashMap<String, String>();
+		
+		map.put(strDate, strPlan);
 	}
 	
+	public void searchPlan(String strDate) {
 		
+		HashMap<String, String> map = new HashMap<String, String>();
+		
+		System.out.println(map.containsKey(strDate));
+	}
 }
-
-
-
--- src/Prompt.java
+++ src/Prompt.java
@@ -3,80 +3,107 @@ import java.util.Scanner;
 
 public class Prompt {
 
-	private final static String PROMPT="cal> ";
-	
+	private final static String PROMPT = "cal> ";
+
 	public void printMenu() {
 		System.out.println("---------------------------------");
 		System.out.println("| 1. 일정 등록");
 		System.out.println("| 2. 일정 검색");
-		System.out.println("| 3. 달 보기		");
-		System.out.println("| q.종료 ");
+		System.out.println("| 3. 달력 보기");
+		System.out.println("| q. 종료 ");
 		System.out.println("---------------------------------");
 	}
-	
-	public void runPrompt() {
+
+	public void registerPlan(Calendar cal, Scanner scanner) {
+		int exit;
 		
-		printMenu();
-		while(true) {
-			Scanner scanner = new Scanner(System.in);
-			String cmd = scanner.next();
-			if (cmd.equals("1")) {
-				cmdRegister();
-			} else if (cmd.equals("2")) {
-				cmdSearch();
-			} else if (cmd.equals("3")) {
-				cmdCal(scanner);
-			} else if (cmd.equals("h")) {
-				printMenu();
-			} else if (cmd.equals("q")) {
-				System.out.println("Bye");
+		while (true) {
+			System.out.println("[일정등록]");
+			System.out.println("날짜를 입력하세요 (yyyy-mm-dd)");
+			String date = scanner.nextLine();
+			
+			System.out.println("계획을 입력하세요");
+			String plan = scanner.nextLine();
+			cal.registerPlan(date, plan);
+			System.out.println("저장되었습니다.");
+			
+			System.out.println("메뉴로 돌아가려면 (-1)");
+			exit = scanner.nextInt();
+			if (exit == -1) {
+				runPrompt();
+			}
+		}	
+	}
+	
+	public void searchPlan(Calendar cal, Scanner scanner) {
+		int exit;
+		
+		while (true) {
+			System.out.println("[일정검색]");
+			System.out.println("날짜를 입력하세요 (yyyy-mm-dd)");
+			String date = scanner.nextLine();
+		
+			cal.searchPlan(date);
+			System.out.println("메뉴로 돌아가려면 (-1)");
+			exit = scanner.nextInt();
+			if (exit == -1) {
+				runPrompt();
 			}
 		}
-		
 	}
-		
 	
+	public void checkCalendar(Calendar cal, Scanner scanner) {
 
-	public void cmdRegister() {
-	}
-	public void cmdSearch() {
-	}
-	public void printTerminate() {
-		return;
-	}
-	public void cmdCal(Scanner scanner) {
+		int year = 2014;		// 임의의 값  
+		int month = 1;		// 임의의 값 
 
-		
-		Calender cal = new Calender();
-		
-		int year = 2014;
-		int month = 1;
-		
-		while(true) {
-			
-			System.out.println("달을 입력 하시오 (종료 = -1)");
+		while (true) {
+
+			System.out.println("달을 입력 하시오. (메뉴로 돌아가기 = -1)");
 			System.out.print(PROMPT);
 			month = scanner.nextInt();
-			
+
 			if (month == -1) {
-				break ;
-			} else if (month > 12 || month < -1 || month == 0){
+				runPrompt();
+			} else if (month > 12 || month < -1 || month == 0) {
 				System.out.println("옳바른 값을 입력하시오 (1~12)");
 				continue;
 			} else {
-			
-			System.out.println("년도를 입력 하시오");
-			System.out.print(PROMPT);
-			year = scanner.nextInt();
-			cal.makeCal(year, month);
- 			cal.CalFirst(year, month);
- 			
+
+				System.out.println("년도를 입력 하시오");
+				System.out.print(PROMPT);
+				year = scanner.nextInt();
+				cal.makeCal(year, month);
+				cal.CalFirst(year, month);
+
 			}
-		
 		}
 	}
+
+	public void runPrompt() {
+
+		printMenu();
+		Calendar cal = new Calendar();
+		Scanner scanner = new Scanner(System.in);
+	
+		while (true) {
+
+			String cmd = scanner.next();
+			if (cmd.equals("1")) {
+				registerPlan(cal, scanner);
+			} else if (cmd.equals("2")) {
+				searchPlan(cal, scanner);
+			} else if (cmd.equals("3")) {
+				checkCalendar(cal, scanner);
+			} else if (cmd.equals("4")) {
+				System.out.println("Bye");
+			}
+
+		}
+	}
+
 	public static void main(String[] args) {
-		
+
 		Prompt p = new Prompt();
 		p.runPrompt();
 	}
